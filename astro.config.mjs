a53b1375c6a34f846c1911fa0c0629e1d07568a1
Rajanish Maury <mswati20298@gmail.com>
2026-08-28 20:53:39 +0530

    updated url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://codnexahub.com',
   integrations: [react(), sitemap()],
 
   vite: {
